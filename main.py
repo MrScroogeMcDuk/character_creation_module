3cb1dd672b77a7c72c8a596b7ab209a52501e1c9
Руслан Касымов <marco.markovich@yandex.ru>
2023-12-03 18:46:37 +0300

    Docstrings

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция, которая возвращает нанесенный урон."""
     if char_class == 'warrior':
         return f"""{char_name}
 нанёс урон противнику равный {5 + randint(3, 5)}"""
@@ -15,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция, которая возвращает заблокинованный урон."""
     if char_class == 'warrior':
         return f"""{char_name}
 блокировал {10 + randint(5, 10)} урона"""
@@ -28,6 +31,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция, которая возвращает специальное умение."""
     if char_class == 'warrior':
         return f"""{char_name}
 применил специальное умение "Выносливость {80 + 25}" """
@@ -41,6 +45,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция, которая возвращает команду начать тренировку."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -65,6 +70,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция, которая возвращает класс персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -85,7 +91,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    """Функция, которая запускает программу."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
